693ca542a68da21c2b8349b1ef731f029f9573e6
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-09-09 23:21:57 +0530

    Iterations

---
+++ src/Iterations.java
@@ -0,0 +1,20 @@
+public class Iterations {
+    static void main() {
+//        LOOPS --> WHILE
+        int i =  1;
+        while(i <= 5){ // boolean --> true / false
+//            System.out.println(i);
+            i++;
+        }
+//         DO WHILE
+        do{
+            System.out.println(i);
+            i++;
+        }while(i <= 10);
+
+//        FOR LOOP
+        for(int j = 1; j<= 5; j++){
+            System.out.println(j);
+        }
+    }
+}
