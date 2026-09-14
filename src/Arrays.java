4cc2794aa83d2c0fe090a9372b9b08b3da1cbf90
Avnish-kaushik <kaushikavi7002@gmail.com>
2026-09-14 22:31:32 +0530

    Arrays-1

---
+++ src/Arrays.java
@@ -0,0 +1,46 @@
+public class Arrays {
+    static void main() {
+
+//        1-D Array
+
+//        int [] rollNumbers = new int[8];
+//        rollNumbers[0] = 101;
+//        rollNumbers[1] = 102;
+//        rollNumbers[2] = 103;
+//
+//        System.out.println(rollNumbers[0]);
+//        System.out.println(rollNumbers[1]);
+//        System.out.println(rollNumbers[2]);
+//
+//        System.out.println(rollNumbers.length);
+
+//        int x = 101;
+//        for(int i = 0; i < rollNumbers.length; i++) {
+//            rollNumbers[i] = x;
+//            x++;
+//        }
+//
+//        for(int i = 0; i < rollNumbers.length; i++) {
+//            System.out.println(rollNumbers[i] + " ");
+//        }
+
+//        2-D Array
+
+        int [][] marks = new int[3][3];
+        marks[0][0] = 50;
+        marks[0][1] = 30;
+        marks[0][2] = 90;
+        marks[1][0] = 50;
+        marks[1][1] = 30;
+        marks[1][2] = 90;
+        marks[2][0] = 50;
+        marks[2][1] = 30;
+        marks[2][2] = 90;
+
+        for(int row = 0; row <marks.length; row++){
+            for(int col = 0; col <marks[row].length; col++){
+                System.out.print(marks[row][col]);
+            }
+        }
+    }
+}
